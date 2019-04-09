e0ac45151931415d23c8c72f6f4c081400f84834
SivaRamavajjala <46703935+SivaRamavajjala@users.noreply.github.com>
2019-04-09 12:54:58 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
 print("Hi This is my first attempt to Azure devops.")
-for i in range(0,7):
+for i in range(0,10):
 	print("The i value is :"+str(i))
+print("I have modified test Test.py file a little bit")
