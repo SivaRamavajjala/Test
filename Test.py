47e44d5d1b9e065ecffa63b111b1a8f2947bca64
Ramavarjula <52101467@acsind.com>
2019-04-09 14:02:54 +0530

    Modified Test.py file

--- Test.py
+++ Test.py
@@ -2,3 +2,4 @@ print("Hi This is my first attempt to Azure devops.")
 for i in range(0,10):
 	print("The i value is :"+str(i))
 print("I have modified test Test.py file a little bit")
+print("I am done with build pipeline basic level")
